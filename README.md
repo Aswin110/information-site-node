a16b250107b396876d2d6e8156ec1125d3ec2f13
Aswin110 <aswinashok110@gmail.com>
2023-12-06 12:41:42 +0530

    added 3 pages and able to navigate to each page and if user try to go to any page not lisited 404 page will show

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # information-site-node
+
+## what is this all about
+
+This is avery simple project to understand the working of node.js. 
--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const url = require('url');
 
 http.createServer(function (req, res) {
 	let q = url.parse(req.url, true);
-	let filename = '.'+q.pathname;
+	let filename = '';
+	if (q.pathname === '/') {
+		filename = './index.html';
+	} else {
+		filename = '.'+q.pathname;
+	}
+
 	fs.readFile(filename, function(err, data) {
 		if (err) {
 			res.writeHead(404, {'content-type': 'text/html'});
-			res.write('not found');
+			res.write(filename + q.pathname);
 			return res.end();
 		}
 		res.writeHead(200, {'content-type': 'text/html'});
